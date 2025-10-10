c4e53a904217980eba8dde2b283281756fca6244
fernandoaraneda <fer.araneda@duocuc.cl>
2025-10-10 14:34:28 -0300

    libreria mal escrita

--- frontend/src/pages/GestionOrdenes.jsx
+++ frontend/src/pages/GestionOrdenes.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import apiClient from '../api/axios.js';
-import styles from '../css/GestionOrdenes.module.css'; // Usaremos un nuevo CSS para el modal
+import styles from '../css/gestionordenes.module.css'; // Usaremos un nuevo CSS para el modal
 import { Wrench, Eye, Edit } from 'lucide-react';
 
 // --- Componente Modal para cambiar el estado ---
