ca987cdb80f44c3d0aaade0222ef2a508419982b
Fernando <fer.araneda@duocuc.cl>
2025-11-05 20:53:38 -0300

    ca

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 # -----------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,7 +133,7 @@ USE_TZ = True
 # -----------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # -----------------------------
 # AUTO FIELD
 # -----------------------------
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.http import HttpResponse  # 👈 importar HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
-
+from accounts.views import LoginView
 
 def healthz(request):
     return HttpResponse("OK")
@@ -13,6 +13,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts.urls')),
     path('healthz', healthz),  # 👈 Health Check
+    path('login/', LoginView.as_view(), name="login-legacy"),
 ]
 
 
